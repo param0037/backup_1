416d67f2774f21977ad6ab3b5953e4e869b0fb11
param0037 <93871506+param0037@users.noreply.github.com>
2022-05-22 00:37:52 +0800

    Add files via upload

---
+++ srcs/classes/GPU_Tensor.h
@@ -0,0 +1,24 @@
+/**
+*    ---------------------------------------------------------------------
+*    Author : Wayne Anderson
+*   Date   : 2021.04.16
+*    ---------------------------------------------------------------------
+*    This is a part of the open source program named "DECX", copyright c Wayne,
+*    2021.04.16
+*/
+
+
+#ifndef _GPU_TENSOR_H_
+#define _GPU_TENSOR_H_
+
+
+namespace de
+{
+    template <typename T>
+    class GPU_Tensor
+    {
+
+    };
+}
+
+#endif
