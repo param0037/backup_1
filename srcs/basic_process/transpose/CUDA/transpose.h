a3ad40e3e67eaefb2246ab2da7e60633ff8bd47c
param0037 <93871506+param0037@users.noreply.github.com>
2022-06-14 23:16:57 +0800

    Update transpose.h

--- srcs/basic_process/transpose/CUDA/transpose.h
+++ srcs/basic_process/transpose/CUDA/transpose.h
@@ -1,7 +1,7 @@
 /**
 *    ---------------------------------------------------------------------
-*    Author : Wayne
-*   Date   : 2021.04.16
+*    Author : Wayne Anderson
+*    Date   : 2021.04.16
 *    ---------------------------------------------------------------------
 *    This is a part of the open source program named "DECX", copyright c Wayne,
 *    2021.04.16
