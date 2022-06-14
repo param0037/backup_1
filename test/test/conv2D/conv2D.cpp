6b843b02b6e9526f2184bcfc3adcb0bdb37cc33c
param0037 <93871506+param0037@users.noreply.github.com>
2022-06-14 18:45:23 +0800

    Create conv2D.cpp

---
+++ test/test/conv2D/conv2D.cpp
@@ -0,0 +1,14 @@
+// conv2D.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "conv2D.h"
+
+
+int main()
+{
+    conv2();
+    //dev_conv2();
+    //dev_conv2_fp16();
+    //dev_conv2_mk();
+    return 0;
+}
